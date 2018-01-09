676a7f684bf9e7f1c07de3c999f4fb06a3c615b0
Jason Kleban <jkleban@outlook.com>
2018-01-09 00:34:27 -0500

    v2

--- framework7.d.ts
+++ framework7.d.ts
@@ -1,9 +1,20 @@
 /// <reference path="dom7.d.ts" />
 /// <reference path="template7.d.ts" />
 
+/** 
+ * This all needs filling in!  Ok to steal from the v1 branch, but 
+ *   make sure it's still valid for v2!
+ * 
+ * 1) Make smaller commits at first.
+ * 2) Comments from the docs would be nice - gotta keep them up as they're updated.
+ * 3) Somebody convince Vladimir on the virtues of typescript as a source language!
+ **/
+
 declare module "framework7" {
     interface Framework7Parameters {
+        /** App bundle id.  Ex. io.framework7.testapp */
         id? : string
+        /** App name. Can be used by other components, e.g. as the default title for Dialog component. */
         name? : string
         version? : string
         theme? : string
@@ -41,6 +52,15 @@ declare module "framework7" {
     interface Utils {}
     interface Request {}
 
+    interface Panel {}
+
+    interface Statusbar {}
+
+    type KnownEvents = 'init' | 'resize' | 'orientationchange' | 'click' 
+        | 'touchstart:active' | 'touchmove:active' | 'touchend:active' 
+        | 'touchstart:passive' | 'touchmove:passive' | 'touchend:passive'
+        | 'panelOpen';
+
     class Framework7 {
         constructor(options?: Framework7Parameters & ClicksModuleParameters & TouchModuleParameters);
         
@@ -77,12 +97,17 @@ declare module "framework7" {
         Modal : any
         CustomModal : any
     
-        on(event : string, handler : Function) : Framework7
-        once(event : string, handler : Function) : Framework7
-        off(event : string, handler : Function) : Framework7
-        off(event : string) : Framework7
-        emit(event : string, ... args : any[]) : Framework7
+        on(event : 'panelOpen', handler : (panel : Panel) => void) : Framework7
+        on(event : KnownEvents | string, handler : Function) : Framework7
+
+        once(event : KnownEvents | string, handler : Function) : Framework7
+        off(event : KnownEvents | string, handler : Function) : Framework7
+        off(event : KnownEvents | string) : Framework7
+        emit(event : KnownEvents | string, ... args : any[]) : Framework7
         init() : void
+
+        panel : Panel
+        statusbar : Statusbar
     }
 
 	export default Framework7;
