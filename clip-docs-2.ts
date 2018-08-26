8babe51e51e08471a924798435468f865d8156d6
Jason Kleban <jkleban@outlook.com>
2018-08-26 11:33:10 -0400

    Core Autocomplete

--- clip-docs-2.ts
+++ clip-docs-2.ts
@@ -5,7 +5,7 @@ const rows = clipboardy.readSync();
 const transformed = rows.split('\n')
     .map(row => {
         const cells = row.split('\t').map(cell => (cell || '').replace(/\n/g, ' ').trim());
-        return `            /** ${cells[1]} */\n            ${cells[0]}`;
+        return `            /** ${cells[1]} */\n            ${cells[0].replace(/^.*\./g, '')}`;
     })
     .join('\n');
 
