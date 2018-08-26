1e84d796c2dda024ae07c4292b34ce4547651cb3
Jason Kleban <jkleban@outlook.com>
2018-08-26 15:48:42 -0400

    Gauge

--- framework7.d.ts
+++ framework7.d.ts
@@ -847,7 +847,9 @@ declare module "framework7" {
             /** Additional css class to add. */
             cssClass: string
             /** Object with events handlers.. */
-            on: object
+            on: {
+                [event in keyof Events] : Function
+           }
         }
 
         export interface Events {
@@ -1043,38 +1045,96 @@ declare module "framework7" {
     export interface Framework7AppEvents extends Form.AppEvents {}
 
     export namespace Gauge {
-        export interface Gauge {
-            // TODO: fill in?
+        export interface Gauge extends EventManagement<Events> {
+            /** Link to global app instance */
+            app : Framework7
+            /** Gauge HTML element */
+            el : HTMLElement
+            /** Dom7 instance with gauge HTML element */
+            $el : Dom7.Dom7
+            /** Gauge generated SVH HTML element */
+            gaugeSvgEl : HTMLElement
+            /** Dom7 instance with generated SVH HTML element */
+            $gaugeSvgEl : Dom7.Dom7
+            /** Gauge parameters */
+            params : Parameters
+
+            /** Update/rerender gauge SVG element according to passed parameters. It accepts object with same parameters required for gauge initialization. You can pass only parameters that needs to be updated */
+            update(parameters : Parameters) : void
+            /** Destroys gauge instance */
+            destroy() : void
         }
 
         export interface Parameters {
-            // TODO: fill in?
+            /** Gauge element. HTMLElement or string with CSS selector of gauge element. Generated SVG will be inserted into this element. */
+            el: HTMLElement | CssSelector
+            /** Gauge type. Can be circle or semicircle. (default circle) */
+            type?: string
+            /** Gauge value/percentage. Must be a number between 0 and 1. (default 0) */
+            value?: number
+            /** Generated SVG image size (in px). (default 200) */
+            size?: number
+            /** Gauge background color. Can be any valid color string, e.g. #ff00ff, rgb(0,0,255), etc.. (default transparent) */
+            bgColor?: string
+            /** Main border/stroke background color. (default #eeeeee) */
+            borderBgColor?: string
+            /** Main border/stroke color. (default #000000) */
+            borderColor?: string
+            /** Main border/stroke width. (default 10) */
+            borderWidth?: string
+            /** Gauge value text (large text in the center of gauge). (default null) */
+            valueText?: string
+            /** Value text color. (default #000000) */
+            valueTextColor?: string
+            /** Value text font size. (default 31) */
+            valueFontSize?: string
+            /** Value text font weight. (default 500) */
+            valueFontWeight?: string
+            /** Gauge additional label text. (default null) */
+            labelText?: string
+            /** Label text color. (default #888888) */
+            labelTextColor?: string
+            /** Label text font size. (default 14) */
+            labelFontSize?: string
+            /** Label text font weight. (default 400) */
+            labelFontWeight?: string
+            /** Object with events handlers.. */
+            on: {
+                [event in keyof Events] : Function
+            }
         }
 
         export interface Events {
-            // TODO: fill in?
+            /** Event will be triggered right before Gauge instance will be destroyed. As an argument event handler receives Gauge instance */
+            beforeDestroy: (dialog : Gauge) => void
         }
     
         export interface DomEvents {
-            // TODO: fill in?
+            /** Event will be triggered right before Gauge instance will be destroyed */
+            'gauge:beforedestroy': () => void
         }
 
         export interface AppMethods {
-            // TODO: fill in?
+            /** create Gauge  instance */
+            create(parameters : Parameters) : Gauge;
+            /** destroy Gauge  instance */
+            destroy(el : HTMLElement | CssSelector | Gauge) : void;
+            /** get Gauge  instance by HTML element */
+            get(el : HTMLElement | CssSelector) : Gauge;
+            /** update/rerender Gauge SVG according to passed parameters */
+            update(parameters : Parameters) : Gauge;
         }
     
         export interface AppEvents {
-            // TODO: fill in?
+            /** Event will be triggered right before Gauge instance will be destroyed. As an argument event handler receives Gauge instance */
+            gaugeBeforeDestroy: (dialog : Gauge) => void
         }
     }
-    export interface Framework7Params {
-        // TODO: fill in?
-    }
+    export interface Framework7Params { }
     export interface Framework7 {
-        // TODO: fill in?
-        // Gauge: Gauge.AppMethods
+        gauge: Gauge.AppMethods
     }
-    //export interface Framework7AppEvents extends Gauge.AppEvents {}
+    export interface Framework7AppEvents extends Gauge.AppEvents {}
 
     export namespace Grid {
         export interface Grid {
